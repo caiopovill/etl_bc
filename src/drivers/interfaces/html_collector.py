92820ac5510785d79a2ed78b8c09335ac1e26ced
caiopovill <caiop_08@hotmail.com>
2022-10-15 12:48:54 -0300

    feat: Finishing implementing transform stage

--- src/drivers/interfaces/html_collector.py
+++ src/drivers/interfaces/html_collector.py
@@ -1,10 +1,8 @@
-from typing import List, Dict
 from abc import ABC, abstractmethod
-
+from typing import List, Dict
 
 class HtmlCollectorInterface(ABC):
 
     @abstractmethod
     def collect_essential_information(self, html: str) -> List[Dict[str, str]]:
         pass
-    
